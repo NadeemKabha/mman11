ec289cc950a48adca65faa86a7a9a7fadbbe6f3e
Nadeem Kabha <nadeem.kabha@gmail.com>
2023-04-02 02:31:32 +0300

    added file reader

--- count_bits.c
+++ count_bits.c
@@ -1,25 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 int count_bits(unsigned long int num);
-void main()
+int main()
 {
     unsigned long int num;
-    printf("Please enter a binary number: ");
-    scanf("%lu", &num);
-    printf("Count is %d\n", count_bits(num));
+    int mycnt = 0;
+    FILE *file = fopen("input", "r");
+    if (!file) 
+    {
+        printf("file can't be read\n");
+        return 1;
+    }
+    while (fscanf(file, "%d", &num) == 1)
+    {
+        if ((mycnt = count_bits(num)) == -1)
+        {
+            printf("Bad input!");
+            return 1;
+        }
+        mycnt = count_bits(num);
+        printf("%d\n", mycnt);
+    }
+    // printf("Please enter a binary number: ");
+    // scanf("%lu", &num);
+    // if ((mycnt = count_bits(num)) == -1)
+    // {
+    //     printf("Bad input!");
+    //     return 1;
+    // }
+
+    // printf("Count is %d\n", mycnt);
+    return 0;
 }
 
 int count_bits(unsigned long int num)
 {
-    // int ind = 0;
     int cnt = 0;
     while (num > 0)
     {
+        if (num % 10 != 0 && num % 10 != 1)
+        {
+            return -1;
+        }
         if (num % 10)
-            {
-                cnt++;
-            }
-            // ind++;
-        
+        {
+            cnt++;
+        }
         num /= 100;
     }
     return cnt;
